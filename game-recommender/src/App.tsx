bc19084125c1b4d734fd9614e396da73d2bfba6d
Dali Guerfali <daliguerfali@gmail.com>
2023-07-28 23:15:42 +0100

    Vite/React-TS/Tailwind/DaisyUI Boilerplate

---
+++ game-recommender/src/App.tsx
@@ -0,0 +1,13 @@
+//import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className="text-5xl font-bold">Vite/React-TS/Tailwind/DaisyUI Boilerplate</h1>
+    </>
+  )
+}
+
+export default App
